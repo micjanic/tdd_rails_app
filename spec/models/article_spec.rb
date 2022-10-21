32d073b0718da0c77a7282b590e9ee5af3ef8529
Michael Janicki <mjanicki@sittercity.com>
2022-10-20 22:43:04 -0500

    factorybot

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  it 'tests article object' do
+    #article = Article.create({title: 'Sample article', content: 'Sample content'})
+    article = FactoryBot.create(:article)
+    expect(article.title).to eq('Sample article')
+  end
+end
